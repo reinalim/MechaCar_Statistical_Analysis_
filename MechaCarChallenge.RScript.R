dff13875f0f33b919a7f805f428bdff0a3bea936
reinalim <72936144+reinalim@users.noreply.github.com>
2021-02-14 17:37:01 -0500

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -11,3 +11,14 @@ lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaC
 
 # Summary 
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar) ) 
+
+# Deliverable 2
+
+# Import the csv file
+Suspension_Coil <- read.csv (file='Suspension_Coil (1).csv',check.names=F,stringsAsFactors = F)
+
+# Create Summary 
+total_summary <- Suspension_Coil %>% summarize (Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
+# Create Lot Summary
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
